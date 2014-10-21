0f70d3d661dba8bb0a6fc6ab5ac8ddd6a936a87c
Daniel Duarte Mendes <danielmd3000gmail.com>
2014-10-21 16:16:28 +0200

    Simple NodeJS Socket.io Client

---
+++ socket.io-agent.js
@@ -0,0 +1,14 @@
+/**
+ * Created by duartda on 21/10/2014.
+ */
+var io = require('socket.io-client'),
+    socket = io.connect('http://localhost:8080');
+
+
+socket.on('connect', function () {
+    console.log("socket connected");
+});
+socket.emit('private message', { user: 'me', msg: 'whazzzup?' });
+socket.on('connect_error', function (error) {
+    console.log(error)
+});
